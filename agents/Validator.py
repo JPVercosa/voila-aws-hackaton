8acb78194ed2155917009289f83337e3437d5fdb
João Verçosa <jpkqvercosa@hotmail.com>
2025-07-20 12:42:42 -0300

    Alterando agente Validator

--- agents/Validator.py
+++ agents/Validator.py
@@ -3,16 +3,21 @@ from strands import Agent, tool
 from utils.novaModel import NOVA_MODEL
 from strands_tools import retrieve
 from pydantic import BaseModel
+from enum import Enum
+from memory.AgentsMemory import memory
 
 VALIDATION_PROMPT = """You are a Validator Agent responsible for validating clauses extracted from documents.
 You will receive a set of clauses and a context in which the validation is being performed.
 Your task is to validate each clause against the context and after comparing them, /
 create a veredict if the proccess can continue or if the clauses need to be revised.
 """
+class ValidationStatus(str, Enum):
+    valid = "valid"
+    invalid = "invalid"
 
 class ValidationResult(BaseModel):
     clause: str
-    status: str
+    status: ValidationStatus
     message: str
 
 class ValidatorAgent:
@@ -37,9 +42,13 @@ class ValidatorAgent:
         Returns:
             dict: A JSON object with the validation status of each clause.
         """
+        memory.set("actual_agent", "Validator")
+        memory.set("actual_tool", "compare")
         if not clauses or not isinstance(clauses, list):
+            
             return {"error": "No clauses provided for validation."}
         if not context:
+            
             return {"error": "No context provided for validation."}
 
         # Create a prompt and use LLM to validate each clause
@@ -55,29 +64,45 @@ class ValidatorAgent:
                 "status": response.status,
                 "message": response.message
             })
-        return validation_results
+
+        # Return only the valid results
+        validation_results = [result for result in validation_results if result['status'] == ValidationStatus.valid]
+        if not validation_results:
+            
+            return {"error": "No valid clauses found after validation."}
+
+        answer = "The following clauses are valid and can be used to create the final answer:\n"
+        for result in validation_results:
+            
+            answer += f"- {result['clause']}\n"
+
+        
+        memory.set("valid_clauses", answer)
+        return answer
 
     def __call__(self):
+        memory.set("actual_agent", "Validator")
         return self.agent
 
 
 
 @tool
-def validate_agent(clauses: dict, context: str) -> str:
+def validate_agent(context: str) -> str:
     """
-    Receives a set of clauses and the context in which the validation is being performed.
-    Validates the clauses and returns a message indicating the validation status.
+    Validate the clauses against the provided context.
+    This function retrieves the clauses from memory and validates them using the ValidatorAgent.
+    If the clauses are valid, it returns a message indicating that the validation was successful.
+    If the clauses are invalid or if there is an error, it returns an appropriate error message.
 
     Args:
-        clauses (list[dict]): A list of dictionaries containing clauses to validate.
         context (str): The context in which the validation is being performed.
 
     Returns:
         str: A message indicating the validation status of the clauses.
     """
 
-    # print(clauses)
-
+    memory.set("actual_agent", "Validator")
+    clauses = memory.get("top_clauses") or []
     if not clauses or not isinstance(clauses, list):
         return "No clauses provided for validation."
     if not context:
@@ -127,51 +152,9 @@ if __name__ == "__main__":
         "clause_text": "Estamos comprometidos em cumprir nossas obriga\u00e7\u00f5es de conformidade relacionadas aos impactos ambientais de nossas opera\u00e7\u00f5es e \u00e0 prote\u00e7\u00e3o do meio ambiente, incluindo a preven\u00e7\u00e3o da polui\u00e7\u00e3o por meio da ado\u00e7\u00e3o de controles apropriados.",
         "area": "compliance",
         "relevance": 0.9
-      },
-      {
-        "section_title": "Pol\u00edtica Ambiental Capgemini",
-        "clause_text": "Identifiquem e implementem iniciativas de economia de energia em nossas instala\u00e7\u00f5es para reduzir o consumo de energia do Grupo, apoiando a transi\u00e7\u00e3o do Grupo para energia renov\u00e1vel e ajudar a redu\u00e7\u00e3o as emiss\u00f5es de GEE associados.",
-        "area": "operations",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Pol\u00edtica Ambiental Capgemini",
-        "clause_text": "Tenham processos de compras que garantam que nossos fornecedores e parceiros de neg\u00f3cios forne\u00e7am produtos e servi\u00e7os que nos ajudem a atingir os objetivos ambientais do Grupo, particularmente em rela\u00e7\u00e3o \u00e0s emiss\u00f5es de carbono, redu\u00e7\u00e3o do consumo de energia e \u00e1gua, redu\u00e7\u00e3o do impacto na biodiversidade e minimiza\u00e7\u00e3o do desperd\u00edcio adotando os princ\u00edpios da circularidade.",
-        "area": "procurement",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Pol\u00edtica Ambiental Capgemini",
-        "clause_text": "Gerimos a implementa\u00e7\u00e3o desta pol\u00edtica ambiental e os nossos impactos ambientais atrav\u00e9s do nosso sistema de gest\u00e3o ambiental global que est\u00e1 certificado na norma ISO 14001.",
-        "area": "operations",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Pol\u00edtica Ambiental Capgemini",
-        "clause_text": "O desempenho em rela\u00e7\u00e3o aos nossos objetivos e metas ser\u00e1 revisado pelo menos anualmente e reportado publicamente em nosso Relat\u00f3rio Financeiro Anual e em quaisquer Relat\u00f3rios de Sustentabilidade associados. Esta Pol\u00edtica Ambiental ser\u00e1 revista pelo menos uma vez por ano.",
-        "area": "operations",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Adriana Gomes Guimaraes Oestreich",
-        "clause_text": "Esta declara\u00e7\u00e3o de pol\u00edtica se aplica a todos os sites a partir dos quais a Capgemini opera, todos os funcion\u00e1rios, fornecedores e alian\u00e7as mantidas pela Capgemini.",
-        "area": "compliance",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Adriana Gomes Guimaraes Oestreich",
-        "clause_text": "Reconhecemos que nossas atividades de neg\u00f3cios t\u00eam impactos e oportunidades para o meio ambiente e estamos comprometidos em melhorar continuamente nosso desempenho ambiental em rela\u00e7\u00e3o a objetivos e metas relevantes (metas baseadas na ci\u00eancia, quando poss\u00edvel) assim como trabalhar com nossos clientes para ajud\u00e1-los a reduzir seus impactos ambientais.",
-        "area": "operations",
-        "relevance": 0.9
-      },
-      {
-        "section_title": "Adriana Gomes Guimaraes Oestreich",
-        "clause_text": "Estamos comprometidos em cumprir nossas obriga\u00e7\u00f5es de conformidade relacionadas aos impactos ambientais de nossas opera\u00e7\u00f5es e \u00e0 prote\u00e7\u00e3o do meio ambiente, incluindo a preven\u00e7\u00e3o da polui\u00e7\u00e3o por meio da ado\u00e7\u00e3o de controles apropriados.",
-        "area": "compliance",
-        "relevance": 0.9
       }
     ]
-
+    memory.set("top_clauses", example_clauses)
     example_context = "O usuário quer saber sobre a política ambiental da Capgemini."
-    validation_result = validate_agent(clauses=example_clauses, context=example_context)
+    validation_result = validate_agent(context=example_context)
     print(validation_result)  # This would be the output of the agent's processing
