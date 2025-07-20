c2ae00946d0b293e3bae406c6de30294c05103fc
João Verçosa <jpkqvercosa@hotmail.com>
2025-07-20 12:42:24 -0300

    Adicionando arquivo para tools

--- agents/tools/agentsTools.py
+++ agents/tools/agentsTools.py
@@ -1,10 +1,14 @@
 import os
 from strands import tool
+from memory.AgentsMemory import memory
+from utils.normalizeNames import normalize_basename, make_md_name
+
 
 @tool
-def check_status(self, document_name: str) -> str:
+def check_status(document_name: str) -> str:
     """
-    Check if the document has been processed.
+    Check if the document has already been processed and is available in Markdown format.
+    This tool checks if a document with the given name exists in the markdown directory.
     
     Args:
         document_name (str): The name of the document to check.
@@ -12,10 +16,15 @@ def check_status(self, document_name: str) -> str:
     Returns:
         str: A message indicating whether the document has been processed or not.
     """
+    memory.set("actual_tool", "check_status")
     base_dir = os.getcwd()
-    markdown_file = os.path.join(base_dir, "markdown", f"{document_name}.md")
+    base_name = normalize_basename(document_name)
+    markdown_file = make_md_name(base_name)
+    markdown_file_path = os.path.join(base_dir, "markdown", markdown_file)
+    print(f"--------------- Checking status of {markdown_file_path} ---------------")
 
-    if os.path.exists(markdown_file):
-        return f"Document {document_name} has been processed and is available in Markdown format."
+    
+    if os.path.exists(markdown_file_path):
+        return f"Document {markdown_file} has been processed and is available in Markdown format."
     else:
-        return f"Document {document_name} has not been processed yet."
+        return f"Document {markdown_file} has not been processed yet."
